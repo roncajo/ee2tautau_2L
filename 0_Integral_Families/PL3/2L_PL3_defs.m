87c57e7cabcc7fdf6e57eb23cc5530f9a841af67
torres <wjtorresb@unal.edu.co>
2025-12-04 12:34:38 +0100

    add PL3

---
+++ 0_Integral_Families/PL3/2L_PL3_defs.m
@@ -0,0 +1,2 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+{r1 -> Sqrt[s*(-4*m2 + s)]}
---
+++ 0_Integral_Families/PL3/2L_PL3_letters.m
@@ -0,0 +1,4 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+{Log[m2], Log[s], Log[t], Log[u], Log[m2 - t], Log[m2 - u], Log[4*m2 - s], 
+ Log[m2^2 - t*u], Log[(-2*m2 - r1 + s + 2*t)/(-2*m2 + r1 + s + 2*t)], 
+ Log[(2*m2 - r1 - s)/(2*m2 + r1 - s)]}
