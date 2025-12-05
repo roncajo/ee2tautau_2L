ce95077ccbd67b8ccf6f00ebc782386453ed3088
torres <wjtorresb@unal.edu.co>
2025-12-05 09:09:43 +0100

    Add PL3b

---
+++ 0_Integral_Families/PL3b/families0.m
@@ -0,0 +1,24 @@
+(* ::Package:: *)
+
+Kinematics0 = {
+	mmp[p1, p1] -> 0, 
+	mmp[p2, p2] -> 0, 
+	mmp[p3, p3] -> m2,
+	mmp[p1, p2] -> s/2,
+	mmp[p1, p3] -> (u-m2)/2,
+	mmp[p2, p3] -> (t-m2)/2
+}/.u->2m2-s-t
+
+
+(* PL3b *)
+Propagators[50] = {
+	mp2[mm[k1]-mm[p1]],
+	mp2[mm[k1]],
+	mp2[mm[k1]+mm[p2]],
+	mp2[mm[k1]+mm[p2]+mm[p3]]-m2,
+	mp2[mm[k2]+mm[p2]]-m2,
+	mp2[mm[k2]+mm[p2]+mm[p3]],
+	mp2[mm[k2]-mm[k1]]-m2,
+	mp2[mm[k2]],
+	mp2[mm[k2]-mm[p1]]-m2
+}
